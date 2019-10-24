859f91c4a1d12754f462e61ebd5359f2987e1fbb
Mrenoir <44065639+Mrenoir@users.noreply.github.com>
2019-10-24 20:07:24 +0200

    Parsefic
    Comme dit sur discord, pour le moment j'ai fait la récuperation sans les structures que je ferai dans la semaine

---
+++ Scraper/src/parse.c
@@ -0,0 +1,75 @@
+#include "define.h"
+/**
+ Fonction pour r�cuperer les action c'est a dire le nom de l'action et son url.
+ number = permet de recuperer le num�ro de l'action
+ param_name permet de savoir ce que vont r�cuperer ,c'est a dire le nom de l'action ou l'url.
+
+ **/
+char *getAction(char *param_name, int number)
+{
+    FILE *fp;
+    char line[10000];
+    char *name = malloc(sizeof(char) * 255);
+    char *param;
+    char param_temp[25];
+    int i = 0;
+    int end = 1;
+    int num = 0;
+    fp = fopen("../files/param.sconf", "r");
+    if (name != NULL && fp != NULL)
+    {
+        param = malloc(sizeof(char) * 2550);
+        name = param_name;
+        printf(" name  :  %s \n ", name);
+
+        while (fgets(line, 255, fp) != NULL)
+        {
+
+            if ((strstr(line, name) != NULL) && strchr(line, '>'))
+            {
+                printf(" num =   %d \n ", num);
+
+                if (num == number)
+                {
+                    strcpy(param, (strchr(line, '>') + 2));
+                }
+
+                if (strchr(param, '}') && num == number)
+                {
+                    while (end)
+                    {
+
+                        if (param[i] == '}')
+                        {
+                            param_temp[i] = '\0';
+                            end = 0;
+                        }
+                        else
+                        {
+                            param_temp[i] = param[i];
+                        }
+                        i++;
+                    }
+                }
+                if (num == number)
+                {
+
+                    free(param);
+                    char *param;
+                    param = malloc(sizeof(char) * (strlen(param_temp) + 2));
+                    strcpy(param, param_temp);
+                    printf(" param : %s \n ", param);
+                    return param;
+                }
+                num++;
+            }
+        }
+    }
+    else
+    {
+        return NULL;
+    }
+    free(param);
+    free(name);
+    fclose(fp);
+}
---
+++ Scraper/src/parse.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock.h>
+#include <string.h>
+#include <time.h>
+char *getAction(char *, int);
+/**
+typedef struct Action
+{
+    char * name;
+    char * url;
+    int  numberAction;
+    char ** action;
+}Action;
+**/
